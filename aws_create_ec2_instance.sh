c1b3797c77c5cf6650024a4c3703e509edbb6ee1
Muhammad Huzaifa Tariq <huzaifatariq14@gmail.com>
2026-02-19 02:31:28 +0500

    Update EC2 instance launch script for instance ID output

--- aws_create_ec2_instance.sh
+++ aws_create_ec2_instance.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-INSTANCE_TYPE="t2.micro"
 AMI_ID="ami-0731becbf832f281e"
+INSTANCE_TYPE="t2.micro"
 KEY_NAME="my_private_key"
 SECURITY_GROUP_ID="sg-0115758fbc5819a09"
 INSTANCE_NAME="HuzaifaTariqs-Instance"
 
-aws ec2 run-instances \
+INSTANCE_ID=$(aws ec2 run-instances \
   --image-id $AMI_ID \
   --instance-type $INSTANCE_TYPE \
   --key-name $KEY_NAME \
   --security-group-ids $SECURITY_GROUP_ID \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" \
+  --query "Instances[0].InstanceId" \
+  --output text) \
   >/dev/null 2>&1
 
-echo "✅ EC2 instance '$INSTANCE_NAME' launched successfully."
+echo "✅ EC2 instance launched: '$INSTANCE_NAME' with id $INSTANCE_ID"
